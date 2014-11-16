345a34a41ffd29c92c63c5af4fc7c7523b3c4c8a
maryanow <imaryanow@yahoo.com>
2014-11-16 11:29:12 -0800

    Update LanguageModel.java

--- LanguageModel.java
+++ LanguageModel.java
@@ -2,7 +2,7 @@
  * LanguageModel.java
  *
  * Implements methods for training a language model from a text file,
- * writing a vocabulary, and randomly completing sentences 
+ * writing a vocabulary, and completes sentences based on training.
  * 
  * Evan Ricks & Iain Maryanow
  * January 2014
@@ -20,28 +20,20 @@ public class LanguageModel
     HashMap<String,Double> p;         // maps ngrams to conditional probabilities
     ArrayList<String> vocab;          // stores the unique words in the input text
     int maxOrder;                     // maximum n-gram order to compute
-    java.util.Random generator;       // a random number generator object
+    java.util.Random generator;       // random number generator object
 
-    // Constructor
-        
-    // LanguageModel
-    // Preconditions:
-    //    - textFilename is the name of a plaintext training file
-    //    - maxOrder is the maximum n-gram order for which to estimate counts
-    //  - generator is java.util.Random object
-    //  - vocabFilename is the name where the vocab file will be written
-    //        vocabFilename can also be null
-    //  - countsFilename is the name where the counts will be written
-    //        countsFilename can also be null
-    // Postconditions:
-    //  - this.p maps ngrams (h,w) to the the maximum likelihood estimates 
-    //    of P(w|h) for all n-grams up to maxOrder 
-    //  - this.vocab contains each word appearing in textFilename exactly once
-    //    in the order they appear in textFilename
-    //  - If vocabFilename is non-null, the vocabulary words are printed to it, one per line, in order
-    //  - If countsFilename is non-null, the ngram counts words are printed to countsFilename, 
-    //    each line has the ngram, then a tab, then the number of times that ngram appears
-    //    (within a line order matters, but the order of the lines themselves doesn't) 
+    /*  - textFilename is the name of a plaintext training file
+    *   - maxOrder is the maximum n-gram order for which to estimate counts
+    *   - vocabFilename is the name where the vocab file will be written
+    *        vocabFilename can also be null
+    *   - countsFilename is the name where the counts will be written
+    *        countsFilename can also be null
+    *
+    *   - this.p maps ngrams (h,w) to the the maximum likelihood estimates 
+    *        of P(w|h) for all n-grams up to maxOrder 
+    *   - this.vocab contains each word appearing in textFilename exactly once
+    *        in the order they appear in textFilename
+    */
     
    public LanguageModel(String textFilename, int maxOrder, java.util.Random generator, String vocabFile, String countsFile) 
    {
@@ -52,15 +44,16 @@ public class LanguageModel
       p = new HashMap<String,Double>();
       vocab = new ArrayList<String>();        
 
-      try(Scanner input = new Scanner(new File(textFilename))) { //check for valid source text file
+      try(Scanner input = new Scanner(new File(textFilename))) { // Checks for valid source text file
          getCounts(input, ngramCounts, historyCounts, vocab, maxOrder);
       }
       catch(FileNotFoundException ex) {
          System.out.println("Error: Unable to open file " + textFilename);
          System.exit(1);
       }
-      saveVocab(vocabFile); // will print each word from vocab to the file given if it is not null
-      saveCounts(countsFile, ngramCounts); //will print each key, a tab, and then the key's value to the file given if it is not null
+
+      saveVocab(vocabFile); // Will print each word from vocab to the file given if it is not null
+      saveCounts(countsFile, ngramCounts); // Will print each key, a tab, and then the key's value to the file given if it is not null
       convertCountsToProbabilities(ngramCounts, historyCounts);
    }
 
@@ -68,35 +61,38 @@ public class LanguageModel
       return maxOrder;
    }
 
-    //  - Until </s> or <fail> is drawn:
-    //    1) Draw a new word w according to P(w|h)
-    //    2) Print a space and then w
-    //    3) w is added to the history h
-    //   Once </s> or <fail> is reached, print that token and then a newline
-    // Notes:
-    //  - Call randomNextWord to draw each new word
+    /*  - Until </s> or <fail> is drawn:
+    *     1) Draw a new word w according to P(w|h)
+    *     2) Print a space and then w
+    *     3) w is added to the history h
+    *  - Once </s> or <fail> is reached, print that token and then a newline
+    *  - Call randomNextWord to draw each new word
+    */
+
    public void randomCompletion(ArrayList<String> history, int order) {
       boolean flag = true;
       ArrayList<String> tempHistory = new ArrayList<String>(history);
       String wordDrawn; 
       while (flag) {
-         wordDrawn = randomNextWord(tempHistory, order); //stores the word that will be drawn from randomNextWord given history and order
-         if (wordDrawn.equals("</s>") || wordDrawn.equals("<fail>")) { //stops while loop when end of sentence or fail is reached
+         wordDrawn = randomNextWord(tempHistory, order); // Stores the word that will be drawn from randomNextWord given history and order.
+         if (wordDrawn.equals("</s>") || wordDrawn.equals("<fail>")) { // Stops while loop when end of sentence or fail is reached.
             flag = false; 
          }
          System.out.print(" " + wordDrawn);
-         tempHistory.add(wordDrawn); //adds drawn word to the end of the temporary history array list
+         tempHistory.add(wordDrawn); // Adds drawn word to the end of the temporary history array list.
       }
       System.out.println();
    }
 
-   private void saveVocab(String vocabFilename) { //if a text file is provided in the command line arguments, will write all vocab words from the arraylist to the file, line by line
-      if(vocabFilename != null) { //skips method if no text file is provided
-         try {//tries to write to file
+   private void saveVocab(String vocabFilename) { // If a text file is provided in the command line arguments, will write all vocab words from the arraylist to the file, line by line
+      if(vocabFilename != null) { 
+         try {
             PrintWriter toFile = new PrintWriter(vocabFilename);
+
             for (int i = 0; i < vocab.size(); i++) {
                toFile.println(vocab.get(i));
             }
+
             toFile.close();
          }
          catch(FileNotFoundException ex) { 
@@ -110,9 +106,11 @@ public class LanguageModel
       if (countsFilename != null) {
          try { 
             PrintWriter toFile = new PrintWriter(countsFilename);
+
             for(Map.Entry<String,Integer> map : ngramCounts.entrySet()) {
                toFile.println(map.getKey() + "\t" + map.getValue());
             }
+
             toFile.close();
          }
          catch(FileNotFoundException ex) {
@@ -122,46 +120,54 @@ public class LanguageModel
       }
    }
 
-    //  - order is the order of n-gram to use 
+    //  - Order is the order of n-gram to use 
     //      (i.e. no more than n-1 history words)
     //  - If no word follows that history for the specified order, return "<fail>"
-   private String randomNextWord(ArrayList<String> history, int order) { //draws a random next word based on a random number generator given a history and order
+   private String randomNextWord(ArrayList<String> history, int order) { // Draws a random next word based on a random number generator given a history and order
       double randomNum = this.generator.nextDouble();
       double cumulativeSum = 0;
-      String key = this.arrayToString(history); //converts history to a string
-      if(history.size() >= order) { //trims history if greater than order size - 1
+      String key = this.arrayToString(history); 
+
+      if (history.size() >= order) { // Trims history if greater than order size - 1
          key = this.arrayToString(history.subList(history.size() - order + 1, history.size()));
       }
-      for(int i = 0; i < vocab.size() - 1; i++) {//adds probabilities of all vocab words happening after the history
-         if(this.p.get(key + " " + vocab.get(i)) != null) {
+
+      for (int i = 0; i < vocab.size() - 1; i++) { // Adds probabilities of all vocab words happening after the history
+         if (this.p.get(key + " " + vocab.get(i)) != null) {
             cumulativeSum += this.p.get(key + " " + vocab.get(i));
          }
-         if(cumulativeSum > randomNum) {//returns the current vocab word when the probabilities becomes greater than the random number generated
+
+         if (cumulativeSum > randomNum) { // Returns the current vocab word when the probabilities becomes greater than the random number
             return vocab.get(i);     
          }
       }
-      if(cumulativeSum == 0) {//returns fail if no words ever follow the given history
+
+      if (cumulativeSum == 0) { // Returns <fail> if no words ever follow the given history
          return "<fail>";
       }
       
-      return vocab.get(vocab.size() - 1); //returns last vocab word in the arraylist if the cumulative probabilities never reach the random number generated (for example when the random number generated is or is very close to 1)
+      return vocab.get(vocab.size() - 1); // Returns last vocab word in the arraylist if the cumulative probabilities never reach the random number generated (for example when the random number generated is or is very close to 1)
    } 
 
     //  - ngramCounts.get(ngram) contains the number of times that ngram appears in the input
     //      ngram must be 2+ words long (e.g. "<s> i")
     //  - historyCounts.get(history) contains the number of times that ngram history appears in the input
     //      histories can be a single word (e.g. "<s>")
-   private void getCounts(java.util.Scanner input,HashMap<String,Integer> ngramCounts,HashMap<String,Integer> historyCounts,ArrayList<String> vocab,int maxOrder) {
+
+   private void getCounts(java.util.Scanner input, HashMap<String,Integer> ngramCounts, HashMap<String,Integer> historyCounts, 
+                          ArrayList<String> vocab, int maxOrder) {
       String line;
-      HashSet<String> tempVocab = new HashSet<String>(); //create a hash set to pass the ngrams through to check for duplicates
-      while(input.hasNextLine()) {
+      HashSet<String> tempVocab = new HashSet<String>(); // Create a hash set to pass the ngrams through to check for duplicates
+
+      while (input.hasNextLine()) {
          line = input.nextLine();
          int lineLength = line.split(" ").length;
          String phrase = "";
          int lineCounter = 0;
          int phraseCounter = 0;
-         while(lineCounter < lineLength - 1) {
-            if(phraseCounter >= maxOrder || phraseCounter + lineCounter >= lineLength) { //increments lineCounter if the ngram length becomes greater than the maxOrder or extends past the end of the line
+
+         while (lineCounter < lineLength - 1) {
+            if (phraseCounter >= maxOrder || phraseCounter + lineCounter >= lineLength) { //increments lineCounter if the ngram length becomes greater than the maxOrder or extends past the end of the line
                lineCounter++;
                phraseCounter = 0;
                phrase = "";
@@ -169,12 +175,12 @@ public class LanguageModel
             
             phrase += " " + line.split(" ")[lineCounter + phraseCounter];
             phrase = phrase.trim();
-            incrementHashMap(historyCounts, phrase); //increments value of the phrase stored in historyCounts
+            incrementHashMap(historyCounts, phrase); // Increments value of the phrase stored in historyCounts
             
-            if(phraseCounter > 0) {
-               incrementHashMap(ngramCounts, phrase); //increments value of the phrase stored in ngramCounts if the phrase is more than 1 word
+            if (phraseCounter > 0) {
+               incrementHashMap(ngramCounts, phrase); // increments value of the phrase stored in ngramCounts if the phrase is more than 1 word
             }
-            if(phraseCounter == 0 && !tempVocab.contains(phrase)) { //adds new word to the arraylist vocab if the phrase is one word and is not already in the vocab
+            if (phraseCounter == 0 && !tempVocab.contains(phrase)) { // Adds new word to the arraylist vocab if the phrase is one word and is not already in the vocab
                vocab.add(phrase);
                tempVocab.add(phrase);
             }
@@ -184,27 +190,30 @@ public class LanguageModel
       }
    }
 
-    //  - this.p.get(ngram) contains the conditional probability P(w|h) for ngram (h,w) 
+    //  - This.p.get(ngram) contains the conditional probability P(w|h) for ngram (h,w) 
     //      only non-zero probabilities are stored in this.p
-   private void convertCountsToProbabilities(HashMap<String,Integer> ngramCounts,HashMap<String,Integer> historyCounts) {
-      for(Map.Entry<String,Integer> map : ngramCounts.entrySet()) {
+
+   private void convertCountsToProbabilities(HashMap<String,Integer> ngramCounts, HashMap<String,Integer> historyCounts) {
+      for (Map.Entry<String,Integer> map : ngramCounts.entrySet()) {
          double probability;
          int index = map.getKey().split(" ").length;
          String key = "";
          
-         for(int i = 0; i < index - 1; i++) {
+         for (int i = 0; i < index - 1; i++) {
             key += " " + map.getKey().split(" ")[i];
          }
+
          key = key.trim();
-         probability = (double)(ngramCounts.get(map.getKey())) / (double)(historyCounts.get(key)); //calculates probability of the word happening after the given history
-         if(probability > 0) {
+         probability = (double)(ngramCounts.get(map.getKey())) / (double)(historyCounts.get(key)); // Calculates probability of the word happening after the given history 
+
+         if (probability > 0) {
             p.put(map.getKey(), probability);
          }      
       }
    }
 
-   private void incrementHashMap(HashMap<String,Integer> map,String key) {
-      if(map.get(key) != null) {
+   private void incrementHashMap(HashMap<String,Integer> map, String key) {
+      if (map.get(key) != null) {
         map.put(key, map.get(key) + 1);
       }
       else {
@@ -214,11 +223,14 @@ public class LanguageModel
 
    public static String arrayToString(List<String> sequence) {
       java.lang.StringBuilder builder = new java.lang.StringBuilder();
-      if( sequence.size() == 0 ) {
+
+      if (sequence.size() == 0) {
          return "";
       }
+
       builder.append(sequence.get(0));
-      for( int i=1; i<sequence.size(); i++ ) {
+
+      for (int i=1; i<sequence.size(); i++) {
          builder.append(" " + sequence.get(i));
       }
       
